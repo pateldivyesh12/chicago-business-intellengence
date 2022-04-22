e4ca5b9919f47d4cd80fcdd3c6b9c9b8d530e5f0
Divyesh Patel <89864313+pateldivyesh12@users.noreply.github.com>
2022-04-22 01:46:36 -0500

    Update main.go

--- main.go
+++ main.go
@@ -176,7 +176,7 @@ func main() {
 	//Database application running on Google Cloud Platform.
 	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=/cloudsql/chicago-business-intelligence1:us-central1:mypostgres sslmode=disable port = 5432"
 
-	db, err := sql.Open("postgres1", db_connection)
+	db, err := sql.Open("postgres", db_connection)
 	if err != nil {
 		panic(err)
 	}
